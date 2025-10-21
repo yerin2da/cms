1b366f234f393a8d4fdb072c044ffdbb9426a4f8
yerin <byl06@naver.com>
2025-10-21 16:59:10 +0900

    js datepicker오류 해결

--- include.js
+++ include.js
@@ -1,23 +1,4 @@
 $(document).ready(function () {
-    /*달력 선언*/
-    $(".apply_date_Ym").datepicker({
-        format: "yyyy-mm", // 원하는 포맷 (년-월)
-        startView: 1,     // 월 뷰부터 시작
-        minViewMode: 1,   // 최소 뷰를 월로 제한
-        autoclose: true,   // 선택 후 자동 닫기
-        language: "kr",
-        orientation: "bottom auto" // 아래쪽으로 열리도록 설정
-    });
-
-    /* 달력 선언 년도 - 월별*/
-    $(".apply_date_Y").datepicker({
-        format: "yyyy",          // 연도만 표시
-        minViewMode: 2,          // 'year' 모드로 제한 (0: day, 1: month, 2: year)
-        viewMode: "years",       // 뷰 모드를 연도 기준으로 시작
-        autoclose: true,
-        language: "kr",
-        orientation: "bottom auto"
-    });
 
     // ======= 퍼블리싱을 위해 임시로 적용 =========//
     //th 정렬
